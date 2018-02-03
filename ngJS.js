7df408154e0b53f1ea8473a13ba53d8ef297da54
HeTrickPH <35405843+HeTrickPH@users.noreply.github.com>
2018-02-03 09:06:28 -0600

    Add files via upload

--- ngJS.js
+++ ngJS.js
@@ -1,6 +1,8 @@
 (function() {
 
 angular.module('crapsApp', []).controller('crapsCtrlr', function myCtlr($scope) {
+	
+$scope.op = "plus";
 
 $scope.PassLine = 0;
 $scope.OddPassLine = 0;
@@ -34,15 +36,17 @@ var timer = null,
 	D2_dot,
 	MoneyYouWin = 0;
 	
-$scope.currentBetAmount = 0;
+var ComeOutRollPhase = true;
+	
+$scope.currentBetAmount = 1;
+$scope.come = 0;
 // Not sure?
 $scope.onNumber = 'off';
 $scope.diceNum = "";
 $scope.earn = 0;
 $scope.loose = 0;
 $scope.betOnTheBoard = 0;
-	
-
+$scope.hidebut = false;
 // Initialize values that get "bind"ed
 $scope.cashInWallet = 8000000000;
 $scope.placeNumbers = [];
@@ -51,120 +55,172 @@ $scope.hardWaysNumbers = [];
 hardWaysNumbers.map( hwn => $scope.hardWaysNumbers.push(hwn));
 $scope.hornNumbers = [];
 hornNumbers.map( x => $scope.hornNumbers.push(x) );
- 
-//figger out I cklicked or db cklicked
-function clicked () {
-		debugger;
-		var CBA;
-        clicks++;  //count clicks
-        if(clicks === 1) {
-            timer = setTimeout(function() {
-                CBA = $scope.currentBetAmount; //perform single-click action    
-                clicks = 0;
-            }, 170);
-        } else {
-            clearTimeout(timer);
-           	CBA = $scope.currentBetAmount * (-1);   //perform double-click action
-            clicks = 0;
-		}
-		return CBA;
-}
 
+$scope.addAndSub = function(num){
+	if(num == 1){
+		$scope.op = "plus";
+	}
+	else if(num == 2){
+		$scope.op = "minus";
+	}
+};
+	
 //betting on place bet
-$scope.addToPlaceBet = function(numberToBetOn) {     
-                var placeBet = _.find($scope.placeNumbers, x => x.num == numberToBetOn) ;
-                if (placeBet!=undefined) {
-                    placeBet.amtbet += $scope.currentBetAmount;
-                	$scope.cashInWallet -= $scope.currentBetAmount;
-                }            
-                $scope.currentOp = "done with addtoplacebet..." + numberToBetOn;
+$scope.addToPlaceBet = function(numberToBetOn) {
+	if (ComeOutRollPhase === false) {
+		var placeBet = _.find($scope.placeNumbers, x => x.num == numberToBetOn) ;
+        if (placeBet!=undefined) {
+			placeBet.amtbet += $scope.currentBetAmount;
+			$scope.cashInWallet -= $scope.currentBetAmount;
+        }            
+        $scope.currentOp = "done with addtoplacebet..." + numberToBetOn;
 	BetOnBoard();
+	}
+                
 };
 	
 //betting on Hard Way
 $scope.betOnHardWays = function(hardwaycallout) {
-                var HardWaysbet = _.find(hardWaysNumbers, { 'num' : hardwaycallout });
-                if (HardWaysbet!=undefined) {
-                                HardWaysbet.amtbet += $scope.currentBetAmount;
-                                $scope.cashInWallet -= $scope.currentBetAmount;
-                }
-                $scope.currentOp = "done with betOnHardWays..." + hardwaycallout;
-	BetOnBoard();
+	if (ComeOutRollPhase === false) {
+		var HardWaysbet = _.find(hardWaysNumbers, { 'num' : hardwaycallout });
+        if (HardWaysbet!=undefined) {
+        	HardWaysbet.amtbet += $scope.currentBetAmount;
+        	$scope.cashInWallet -= $scope.currentBetAmount;
+        }
+        $scope.currentOp = "done with betOnHardWays..." + hardwaycallout;
+		BetOnBoard();
+	}
+                
 };
 
 //betting on Horn Bet
 $scope.betOnHorn = function(horncallout) {
-                var HornBet = _.find(hornNumbers, { 'num' : horncallout });
-                if (HornBet!=undefined) {
-                	HornBet.amtbet += $scope.currentBetAmount;
-                	$scope.cashInWallet -= $scope.currentBetAmount;
-                }
-                $scope.currentOp = "done with betOnHorn..." + horncallout;
+	var HornBet = _.find(hornNumbers, { 'num' : horncallout });
+		if (HornBet!=undefined) {
+			HornBet.amtbet += $scope.currentBetAmount;
+			$scope.cashInWallet -= $scope.currentBetAmount;
+		}
+	$scope.currentOp = "done with betOnHorn..." + horncallout;
 	BetOnBoard();
 };
+	
+$scope.betOnAllHorn = function(){
+	hornNumbers.forEach(item => item.amtbet += $scope.currentBetAmount);
+	$scope.cashInWallet -= ($scope.currentBetAmount * hornNumbers.length);
+	$scope.currentOp = "done with betOnAllHorn";
+	BetOnBoard();
+}
 	
 //betting on field bet
 $scope.betOnField = function(){
 	BetOnBoard();
-}
-
+};
+	
 // this is choose $scope.currentBetAmount that I want use
 $scope.chooseCurrentBetAmount = function(value) {
-                $scope.currentBetAmount += value;
+	if($scope.op == "plus"){
+		$scope.currentBetAmount += value;
+	}
+    else if($scope.op == "minus"){
+		$scope.currentBetAmount -= value;
+	}
 };
  
 // This function handles the come out roll or th first roll
 $scope.comeoutRoll = function() {
-				$scope.loose = 0;
-                var DiceDot = newDiceRoll();
-                if (DiceDot == 7 || DiceDot == 11) {
-                                $scope.cashInWallet += $scope.PassLine * 1;
-								MoneyYouWin += $scope.PassLine * 1
-                } else if (DiceDot == 2 || DiceDot ==  3 || DiceDot == 12) {
-                                $scope.PassLine = 0;
-                } else {
-                                $scope.onNumber = DiceDot;
-                                placeWillBeON = "onBtn" + DiceDot;
-                                document.getElementById(placeWillBeON).innerHTML = "<img class='onBut' src='img/onButton.png' width='42px' height='42px'>";
-                                //displayRollDice(true);
-                }
-                calcHardWaysBet();
-                calcHornBet();
-				//Need To ask better Way!!!!!!!
-                hornNumbers.forEach(item => $scope.loose+= item.amtbet);
-				hornNumbers.forEach(item => item.amtbet= 0);
-				if ($scope.loose == 0) {
-					$scope.loose = "";
-					
-				}
-				thatIGot();
+	$scope.loose = 0;
+	var DiceDot = newDiceRoll();
+	if (DiceDot == 7 || DiceDot == 11) {
+		$scope.cashInWallet += $scope.PassLine * 1;
+		MoneyYouWin += $scope.PassLine * 1;
+	} else if (DiceDot == 2 || DiceDot ==  3 || DiceDot == 12) {
+		$scope.PassLine = 0;
+	} else {
+		$scope.onNumber = DiceDot;
+		placeWillBeON = "onBtn" + DiceDot;
+		document.getElementById(placeWillBeON).innerHTML = "<img class='onBut' src='img/onButton.png' width='42px' height='42px'>";
+		$scope.hidebut = true;
+	}
+	calcHardWaysBet();
+	calcHornBet();
+	//Need To ask better Way!!!!!!!
+	hornNumbers.forEach(item => $scope.loose+= item.amtbet);
+	hornNumbers.forEach(item => item.amtbet= 0);
+	if ($scope.loose == 0) {
+		$scope.loose = "";
+	}
+	thatIGot();
 	BetOnBoard();
+	ComeOutRollPhase = false;
 };
 
+//place a pass line bet 
+$scope.addToComeBet = function () {
+	if (ComeOutRollPhase == false) {
+		$scope.come += $scope.currentBetAmount;
+		$scope.cashInWallet -= $scope.currentBetAmount;
+	}
+	BetOnBoard();
+}
 
 // Place a pass line bet
 $scope.addToPassLineBet = function() {
-	var BetAmount = clicked();
-	if ($scope.PassLine != undefined) {
-		$scope.PassLine += BetAmount;
-        $scope.cashInWallet -= BetAmount;
+	if (ComeOutRollPhase) {
+		clicks++;  //count clicks
+        if(clicks === 1) {
+            timer = setTimeout(function() {
+				debugger;
+				//perform single-click action   
+                $scope.PassLine += $scope.currentBetAmount 
+				$scope.cashInWallet -= $scope.currentBetAmount;
+                clicks = 0;
+            }, 250);
+        } else {
+            clearTimeout(timer);
+           	 //perform double-click action
+			$scope.PassLine -= $scope.currentBetAmount 
+			$scope.cashInWallet += $scope.currentBetAmount;
+            clicks = 0;
+		}
 	}
-	$scope.currentOp = "done with Pass Line...Pass Line";
 	BetOnBoard();
 };
- 
+	
+//function AddOrSub(bettingPlace, cashInWallet, currentOp) {
+//        clicks++;  //count clicks
+//       if(clicks === 1) {
+//            timer = setTimeout(function() {
+//				//perform single-click action   
+//                bettingPlace += $scope.currentBetAmount 
+//				cashInWallet -= $scope.currentBetAmount;
+//                clicks = 0;
+//            }, 170);
+//        } else {
+//            clearTimeout(timer);
+//           	 //perform double-click action
+//			bettingPlace += $scope.currentBetAmount 
+//			cashInWallet -= $scope.currentBetAmount;
+//            clicks = 0;
+//		}
+//	if (clicks === 0) {
+//		$scope.currentOp = currentOp;
+//	}
+//	BetOnBoard();
+//};
+	
 // Place a odd pass line bet
 $scope.OddPassLineBet = function() {
-	if ($scope.OddPassLine != undefined) {
-		$scope.OddPassLine += $scope.currentBetAmount;
-        $scope.cashInWallet -= $scope.currentBetAmount;
+	if (ComeOutRollPhase == false) {
+		if ($scope.OddPassLine != undefined) {
+			$scope.OddPassLine += $scope.currentBetAmount;
+			$scope.cashInWallet -= $scope.currentBetAmount;
+		}
+		$scope.currentOp = "done with Pass Line...Odd Pass Line";
 	}
-	$scope.currentOp = "done with Pass Line...Odd Pass Line";
 	BetOnBoard();
 };
 
 $scope.rollDice = function(){
-	debugger;
 		   		$scope.loose = 0;
                 var DiceDot = newDiceRoll();
                 if (DiceDot == 7) {
@@ -177,34 +233,35 @@ $scope.rollDice = function(){
                 	$scope.onNumber = "Off";
                 	document.getElementById(placeWillBeON).innerHTML = "";
 					$scope.currentBetAmount = 0;
-                	//displayRollDice(false);
+                	$scope.hidebut = false;
+					ComeOutRollPhase = false;
                 }
                 else if (DiceDot == $scope.onNumber) {
 					$scope.cashInWallet += ($scope.PassLine * 1);
 					MoneyYouWin += $scope.PassLine * 1;
                 	calcOddPassLineBet(DiceDot);
                 	document.getElementById(placeWillBeON).innerHTML = "";
+					$scope.onNumber = "Off";
 					calcPlaceBet(DiceDot);
-					calcHardWaysbet();
-                	calcHornbet();
-                	//displayRollDice(false);
+					calcHardWaysBet();
+                	calcHornBet();
+					$scope.hidebut = false;
+					ComeOutRollPhase = false;
 				}
-				if (placeBet!=undefined) {
-					calcPlaceBet(DiceDot);
-                	calcHardWaysbet();
-                	calcHornbet();
-				}
+    calcHardWaysBet();
+    calcHornBet();
+	calcComeBet(DiceDot);
 	hornNumbers.forEach(item => $scope.loose+= item.amtbet);
 	hornNumbers.forEach(item => item.amtbet= 0);
 	thatIGot();
 	if ($scope.loose == 0) {
-		$scope.loose = "";
+		$scope.loose = 0;
 	}
+	
 	BetOnBoard();
 };
 
 function BetOnBoard() {
-	debugger;
 	$scope.betOnTheBoard = 0;
 	placeNumbers.forEach(item => $scope.betOnTheBoard+= item.amtbet);
 	hardWaysNumbers.forEach(item => $scope.betOnTheBoard+= item.amtbet);
@@ -285,7 +342,6 @@ function calcHardWaysBet() {
 }
  
 function calcHornBet() {
-                debugger;
                 var Dot = D1_dot + "-" + D2_dot;
                 var HornBet = _.find(hornNumbers, { 'num' : Dot }) ;
                 if (HornBet!=undefined) {
@@ -294,6 +350,20 @@ function calcHornBet() {
 				}
 }
 
+function calcComeBet(DiceDot) {
+	debugger;
+	if (DiceDot == 12 || DiceDot == 2 || DiceDot == 3) {
+		$scope.come = 0;
+	} else if (DiceDot == 7 || DiceDot == 11) {
+		$scope.cashInWallet += $scope.come;
+		MoneyYouWin += $scope.amtbet;
+	} else {
+		var placeBet = _.find($scope.placeNumbers, x => x.num == DiceDot);
+        placeBet.amtbet += $scope.come;
+		$scope.come = 0;
+	}
 	
+}
+
   });
 }) ();
